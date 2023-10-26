e5d378650fb10e863b65e00ce5645d7e45853d85
wenzizone <wenzizone@126.com>
2023-10-26 08:46:22 +0800

    update

---
+++ backend/internal/log/log.go
@@ -0,0 +1,19 @@
+package log
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+)
+
+var logger *slog.Logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+func Infof(format string, args ...any) {
+	msg := fmt.Sprintf(format, args)
+	logger.Info(msg)
+}
+
+func Errorf(format string, args ...any) {
+	msg := fmt.Sprintf(format, args)
+	logger.Error(msg)
+}
--- backend/internal/router/handler/api/api.go
+++ backend/internal/router/handler/api/api.go
@@ -2,14 +2,16 @@ package api
 
 import (
 	"fmt"
+	"github.com/wenzizone/94list/backend/internal/log"
 	"net/http"
 )
 
-type getList struct{}
+func Getlist() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		uPath := r.URL.Path
+		uHeader := r.Header
 
-func (gl *getList) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	uPath := r.URL.Path
-	uHeader := r.Header
-
-	fmt.Fprintf(w, "path: %s, heder: %s", uPath, uHeader)
+		log.Infof("path: %s, header: %s", uPath, uHeader)
+		fmt.Fprintf(w, "path: %s", uPath)
+	})
 }
