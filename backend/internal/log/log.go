76b732493a26a18f4f9e77029681556171fb9b12
wenzizone <wenzizone@126.com>
2023-10-27 18:43:58 +0800

    upddate

--- backend/internal/log/log.go
+++ backend/internal/log/log.go
@@ -9,11 +9,12 @@ import (
 var logger *slog.Logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
 func Infof(format string, args ...any) {
-	msg := fmt.Sprintf(format, args)
+	msg := fmt.Sprintf(format, args...)
+	fmt.Println(args...)
 	logger.Info(msg)
 }
 
 func Errorf(format string, args ...any) {
-	msg := fmt.Sprintf(format, args)
+	msg := fmt.Sprintf(format, args...)
 	logger.Error(msg)
 }
--- backend/internal/router/handler/api/api.go
+++ backend/internal/router/handler/api/api.go
@@ -1,17 +1,23 @@
 package api
 
 import (
-	"fmt"
-	"github.com/wenzizone/94list/backend/internal/log"
-	"net/http"
+  "fmt"
+  "github.com/wenzizone/94list/backend/internal/log"
+  "net/http"
 )
 
-func Getlist() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		uPath := r.URL.Path
-		uHeader := r.Header
+func GetList(w http.ResponseWriter, r *http.Request) {
+  uPath := r.URL.Path
+  uHeader := r.Header
 
-		log.Infof("path: %s, header: %s", uPath, uHeader)
-		fmt.Fprintf(w, "path: %s", uPath)
-	})
+  log.Infof("path: %s, header: %s", uPath, uHeader)
+  fmt.Fprintf(w, "path: %s", uPath)
+}
+
+func GetSign(w http.ResponseWriter, r *http.Request) {
+  uPath := r.URL.Path
+  uHeader := r.Header
+
+  log.Infof("path: %s, header: %s", uPath, uHeader)
+  fmt.Fprintf(w, "path: %s", uPath)
 }
