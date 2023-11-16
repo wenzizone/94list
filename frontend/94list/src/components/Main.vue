8b2f15fec0f7a5b7b3d34eae8370d7683a8a0aad
wenzizone <wenzizone@126.com>
2023-11-16 21:55:58 +0800

    update

--- frontend/94list/src/components/Main.vue
+++ frontend/94list/src/components/Main.vue
@@ -1,51 +1,55 @@
 <template>
-  <a-layout>
-    <a-layout style="margin: 16px 0; padding: 0 24px 24px">
-      <h2>前台解析中心 | 94list-vue-go</h2>
-      <a-layout-content
-          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-        >
-        <a-form
-          name="basic"
-          :label-col="{ span: 8 }"
-          :wrapper-col="{ span: 16 }"
-          autocomplete="off"
-          style="max-width: 600px"
-        >
-          <a-form-item
-            label="链接"
-            name="链接"
-            :rules="[{ required: true, message: '请填入百度网盘分享链接' }]"
-          >
-            <a-input />
-          </a-form-item>
-
-          <a-form-item
-            label="密码"
-            name="密码"
-          >
-            <a-input />
-          </a-form-item>
-
-          <a-form-item
-            label="当前路径"
-            name="当前路径"
-          >
-            <a-input value="/" disabled />
-          </a-form-item>
-
-          <a-form-item>
-            <a-space>
-              <a-button type="primary" html-type="submit">解析链接</a-button>
-              <a-button type="primary">刷新列表</a-button>
-              <a-button type="primary" disabled> 批量下载 </a-button> 
-              <a-button type="primary"> 复制当前地址 </a-button>
-            </a-space>
-            
-          </a-form-item>
-        </a-form>
-      </a-layout-content>
-    </a-layout>
-  </a-layout>
+  <el-space direction="vertical">
+    <el-card shadow="always">
+      <template #header>
+        <div class="card-header">
+          <h2>前台解析中心 | 94list-vue-go</h2>
+        </div>
+      </template>
+      <el-form label-width="auto">
+        <div>
+          <el-form-item label="链接">
+            <el-input />
+          </el-form-item>
+        </div>
+        
+        <el-form-item label="密码">
+          <el-input />
+        </el-form-item>
+        <el-form-item label="当前路径">
+          <el-input value="/" disabled />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">解析链接</el-button>
+          <el-button type="primary">刷新列表</el-button>
+          <el-button type="primary" disabled>批量下载</el-button>
+          <el-button type="primary">复制当前地址</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  
+    <el-card shadow="always">
+      <el-table :data="tableData" border style="width: 100%">
+        <el-table-column prop="date" label="Date" width="180" />
+        <el-table-column prop="name" label="Name" width="180" />
+        <el-table-column prop="address" label="Address" />
+      </el-table>
+    </el-card>
+  </el-space>
 </template>
 
+<style>
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+</style>
--- frontend/94list/src/main.js
+++ frontend/94list/src/main.js
@@ -2,8 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/reset.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
-app.use(Antd).mount('#app')
+app.use(ElementPlus)
+app.mount('#app')
